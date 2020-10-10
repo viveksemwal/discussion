bd69b3809097aaf228ab3205091bb8ae022a9ed3
viveksemwal <Viveksemwal1997@gmail.com>
2020-10-10 22:30:24 +0530

    starting new project

---
+++ src/main/java/com/vivek/discussion/model/Post.java
@@ -0,0 +1,45 @@
+package com.vivek.discussion.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.Instant;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "Cannot left this blank")
+    private String postName;
+
+    @Nullable
+    private String url;
+
+    @Nullable
+    @Lob
+    private String description;
+
+    private int voteCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userid", referencedColumnName = "userid")
+    private User user;
+
+    private Instant createdDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private Subreddit subreddit;
+}
